b6b415413910e21d6a92f2aae764a118eb767cba
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2024-12-30 01:01:06 +0800

    Update shutdown

--- docker/wakeup.py
+++ docker/wakeup.py
@@ -12,12 +12,15 @@ config.read('config.ini')
 client_id = config.get('bafa', 'client_id').strip('"')
 # 主题值
 topic = config.get('bafa', 'topic').strip('"')
+ssh_ip = config.get('openssh', 'ip').strip('"')
+ssh_user = config.get('openssh', 'user').strip('"')
+ssh_password = config.get('openssh', 'password').strip('"')
 # 局域网网络唤醒
 cmd1 = 'wakeonlan ' + config.get('interface', 'mac').strip('"')
 # 发送关闭电脑off给巴法云，更新状态
 cmd2 = '/usr/bin/curl -s "https://api.bemfa.com/api/device/v1/data/3/push/get/?uid=%(uid)s&topic=%(topic)s&msg=off" -w "\n"'%{"uid": client_id, "topic": topic}
 # 局域网连接openssh服务器，进行关机操作
-cmd3 = 'sshpass -p %(password)s ssh -A -g %(user)s@%(ip)s "shutdown -s -t 10"'%{"password": config.get('openssh', 'password').strip('"'), "user": config.get('openssh', 'user').strip('"'), "ip": config.get('openssh', 'ip').strip('"')}
+cmd3 = 'sshpass -p %(password)s ssh -A -g -o StrictHostKeyChecking=no %(user)s@%(ip)s "shutdown -s -t 10"'%{"password": ssh_password, "user": ssh_user, "ip": ssh_ip}
 
 def connTCP():
     global tcp_client_socket
@@ -53,6 +56,16 @@ def Ping():
 connTCP()
 Ping()
 
+# 检测目标PC是否在线
+def check_url(url=ssh_ip, timeout=10):
+	try:
+		socket.setdefaulttimeout(3)
+		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((url, 22))
+		return True
+	except Exception as e:
+		print(f"无法连接到 {url}:{22}，错误：{e}")
+		return False
+
 while True:
 	# 接收服务器发送过来的数据
 	recvData = tcp_client_socket.recv(1024)
@@ -73,8 +86,11 @@ while True:
 					print("正在关闭电脑")
 					print(f"执行命令:{cmd2}")
 					os.system(cmd2)
-					print(f"执行命令:{cmd3}")
-					os.system(cmd3)
+					if check_url():
+						print(f"执行命令:{cmd3}")
+						os.system(cmd3)
+					else:
+						print("目标PC未在线或SSH服务器未开启")
 				except:
 					time.sleep(2)
 		except:
