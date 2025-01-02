35efdd194b5d94485a4a99deb623f61295ea5c61
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-01-02 20:15:52 +0800

    删除发送给巴法云的命令，不需要，是从巴法云接受到数据

--- docker/wakeup.py
+++ docker/wakeup.py
@@ -23,8 +23,6 @@ ssh_user = config.get('openssh', 'user').strip('"')
 ssh_password = config.get('openssh', 'password').strip('"')
 # 局域网网络唤醒
 cmd1 = 'wakeonlan ' + config.get('interface', 'mac').strip('"')
-# 发送关闭电脑off给巴法云，更新状态
-cmd2 = '/usr/bin/curl -s "https://api.bemfa.com/api/device/v1/data/3/push/get/?uid=%(uid)s&topic=%(topic)s&msg=off" -w "\n"'%{"uid": client_id, "topic": topic}
 # 局域网连接openssh服务器，进行关机操作
 cmd3 = 'sshpass -p %(password)s ssh -A -g -o StrictHostKeyChecking=no %(user)s@%(ip)s "shutdown -s -t 10"'%{"password": ssh_password, "user": ssh_user, "ip": ssh_ip}
 
@@ -90,8 +88,6 @@ while True:
 			else:
 				try:
 					print("正在关闭电脑")
-					print(f"执行命令:{cmd2}")
-					os.system(cmd2)
 					if check_url():
 						print(f"执行命令:{cmd3}")
 						os.system(cmd3)
