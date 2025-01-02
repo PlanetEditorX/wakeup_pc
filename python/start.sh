e9a3cb3e9ff928cf731d3fe284e33dbb1c7c53f6
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-01-02 23:53:34 +0800

    Update start.sh

--- python/start.sh
+++ python/start.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
+echo "===================================" >> ./log.txt
+echo "Start Wakeup PC Running with Python" >> ./log.txt
+echo "===================================" >> ./log.txt
 # 清理缓存
+echo "Clear cache......" >> ./log.txt
 rm -rf /var/cache/apk/*
 
 # 创建日志文件
-touch log.txt
+if [ ! -f "log.txt" ]; then
+    echo "The log.txt does not exist and is being created......" >> ./log.txt
+    touch log.txt
+else
+    echo "Find local log.txt" >> ./log.txt
+fi
 
 # 定时任务
+echo "Set crontab......" >> log.txt
 /usr/bin/crontab ./crontab
 
 # 启动 cron 服务
+echo "Start crond service......" >> log.txt
 /usr/sbin/crond
 
 # 等待 cron 启动完成
 sleep 1
 
 # 使wakeup可执行
+echo "Wakeup adds executable permissions......" >> log.txt
 chmod +x ./wakeup
 
 # 运行
+echo "Running wakeup......" >> log.txt
 ./wakeup
