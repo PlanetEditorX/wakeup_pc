39c1bd17016cbf1a3484b0087961d5a813de5bad
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-01-02 21:36:41 +0800

    Update python Dockerfile and start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# 清理缓存
+rm -rf /var/cache/apk/*
+
+# 创建日志文件
+touch log.txt
+
 # 定时任务
-/usr/bin/crontab /crontab
+/usr/bin/crontab ./crontab
 
 # 启动 cron 服务
 /usr/sbin/crond
@@ -9,11 +15,8 @@
 # 等待 cron 启动完成
 sleep 1
 
-# 使Python脚本可执行
-chmod +x /wakeup.py
+# 使wakeup可执行
+chmod +x ./wakeup
 
-# 运行 Python 脚本
-python3 /wakeup.py
-
-# 清理缓存
-rm -rf /var/cache/apk/* && apk cache clean
+# 运行
+./wakeup
